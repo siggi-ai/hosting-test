82a24279a323d7613da2079f7a41b433c551187b
siggi-ai <82755519+siggi-ai@users.noreply.github.com>
2021-08-16 22:01:05 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+  
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", function(req, res) {
+    res.send("HELLO THERE PEOPLE!!!!!");
+});
+
+app.listen(port, function() {
+    console.log(`Happily listening on ${port}`);
+});
